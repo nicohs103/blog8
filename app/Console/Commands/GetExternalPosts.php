9e8c55a73ad78eed9172704767257755544d8d93
Nico Di Marco <ndimarco@avantio.com>
2021-04-19 19:51:02 +0200

    Add cache to welcome page

--- app/Console/Commands/GetExternalPosts.php
+++ app/Console/Commands/GetExternalPosts.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 use App\Models\Post;
+use Illuminate\Support\Facades\Cache;
 
 class GetExternalPosts extends Command
 {
@@ -21,6 +22,9 @@ class GetExternalPosts extends Command
      * @var string
      */
     protected $description = 'Import posts created in a external blogging web';
+
+    const CACHE_KEY = 'welcome_posts_';
+
 
     /**
      * Create a new command instance.
@@ -65,6 +69,8 @@ class GetExternalPosts extends Command
         $response = Http::get($externalBlogUrl . 'posts');
 
         if ($response->successful()) {
+            $this->forgetCache();
+
             return json_decode($response->body());
         }
 
@@ -88,5 +94,17 @@ class GetExternalPosts extends Command
         $post->save();
 
         return;
+    }
+
+    private static function forgetCache()
+    {
+        for ($i = 1; $i < 500; $i++) {
+            $key = self::CACHE_KEY . $i;
+            if (Cache::has($key)) {
+                Cache::forget($key);
+            } else {
+                break;
+            }
+        }
     }
 }
--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\Cache;
 
 class WelcomeController extends Controller
 {
+    const CACHE_KEY = 'welcome_posts_';
+    const CACHE_TTL = 600;
+
     /**
      * Display a listing of the resource.
      *
@@ -14,8 +18,13 @@ class WelcomeController extends Controller
      */
     public function index()
     {
-        return view('welcome', [
-            'lastPosts' => Post::with('user')->orderBy('id', 'DESC')->simplePaginate(10)
-        ]);
+        $page = request()->get('page', 1);
+        $limit = request()->get('limit', 10);
+
+        $welcome_posts = Cache::remember(self::CACHE_KEY . $page, self::CACHE_TTL, function () use ($limit) {
+            return Post::with('user')->orderBy('id', 'DESC')->limit(500)->simplePaginate($limit);
+        });
+
+        return view('welcome', ['lastPosts' => $welcome_posts]);
     }
 }
